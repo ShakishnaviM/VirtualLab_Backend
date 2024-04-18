37b46a9a84313ef17a48f63df062cb01e92c4f89
ShakishnaviM <shakishnavi27@gmail.com>
2024-04-18 09:52:19 +0530

    create signIn API route

--- API/controller/authController.js
+++ API/controller/authController.js
@@ -1,18 +1,14 @@
 import User from "../models/userModel.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../Utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signUp = async (req, res, next) => {
     const { username, email, password, Stream } = req.body;
 
-    console.log(req.body);
-    console.log(req.body.username);
-    console.log(req.body.email);
-    console.log(req.body.password);
-    console.log(req.body.Stream);
-
 
     // Check if password is provided
-    if (!email) {
+    if (!password) {
         return res.status(400).json({ message: "Password is required" });
     }
 
@@ -33,3 +29,26 @@ export const signUp = async (req, res, next) => {
         next(error);
     }
 };
+
+
+export const signIn = async(req, res, next) =>{
+    const{email, password} = req.body;
+
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, 'User not found'));
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401, 'Wrong credentials'));
+
+        const token = jwt.sign({id : validUser._id }, process.env.JWT_SECRET);
+        const {password: hashedPassword, ...rest} = validUser._doc;
+        const expiryDate = new Date(Date.now()+3600000); //1hour
+        res
+            .cookie('access_token', token, {httpOnly:true, expires: expiryDate})
+            .status(200)
+            .json(rest);
+    }catch(error){
+        next(error);
+    }
+}
