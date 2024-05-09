a7f97184f981a11ca411507a79b0c8fabce51744
Loshan20011 <loshan20011@gmail.com>
2024-05-09 12:51:58 +0530

    update-delate-changed

--- API/controller/userController.js
+++ API/controller/userController.js
@@ -8,33 +8,60 @@ export const test = (req, res) => {
     });
 };
 
-//update user
-export const updateUser = async(req, res, next) =>{
-    if(req.user.id !== req.params.id){
-        return next(errorHandler(401, 'You can update only your account!'))
+// Update user
+export const updateUser = async (req, res, next) => {
+  try {
+    // Bcrypt the password if provided
+    let hashedPassword = null;
+    if (req.body.password) {
+      hashedPassword = await bcryptjs.hash(req.body.password, 10);
     }
-    try{
-        if(req.body.password){
-            req.body.password = bcryptjs.hashSync(req.body.password, 15);
-        }
-        const updatedUser = await User.findByIdAndUpdate
-           ( req.params.id,
-            {
-                $set:
-                    {
-                    username: req.body.username,
-                    email: req.body.email,
-                    password: req.body.password,
-                    }
-            },
-            {new :true}
-        );
-        const {password, ...rest} = updatedUser._doc;
-        res.status(200).json(updatedUser);
 
+    // Update user information
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: hashedPassword, // Set the hashed password
+        },
+      },
+      { new: true }
+    );
 
-    }catch{
-        next(error);
-         
+    // Remove password from the response
+    const { password, ...userData } = updatedUser._doc;
+
+    // Send updated user data in the response
+    res.status(200).json(userData);
+  } catch (error) {
+    // Handle errors
+    console.error('Error updating user:', error);
+    next(error);
+  }
+};
+
+export const userDelete = async (req, res, next) => {
+  const id = req.params.id;
+
+  try {
+    // Find the user first by _id
+    const user = await User.findOne({ _id: id });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-}
+
+    const { email } = user;
+
+    // Then delete the user by _id
+    await User.findOneAndDelete({ _id: id });
+
+    // Respond with success message and deleted user's email
+    res.status(200).json({ message: "User deleted successfully", deletedUser: { email } });
+
+  } catch (error) {
+    next(error);
+  }
+};
