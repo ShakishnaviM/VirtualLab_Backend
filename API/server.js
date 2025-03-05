31c178075f54b91a71b04e5862e847e25668e946
ShakishnaviM <shakishnavi27@gmail.com>
2025-03-05 22:49:01 +0530

    add deployment changes

--- API/server.js
+++ API/server.js
@@ -1,4 +1,4 @@
-import './index.js';
-import { createServer } from '@vercel/node';
+import app from './index.js';  // Import the Express app
+import { createServer } from '@vercel/node';  // Vercel's serverless function handler
 
 export default createServer(app);
